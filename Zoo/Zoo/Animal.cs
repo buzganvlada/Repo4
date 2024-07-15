20b96ac128d20dbadd527d261a54ed1b786c41aa
axelpxl33 <akselnordevpxl33@gmail.com>
2024-07-15 13:23:48 +0300

    Add project files.

---
+++ Zoo/Zoo/Animal.cs
@@ -0,0 +1,15 @@
+﻿public abstract class Animal
+{
+    public abstract void MakeSound();
+    
+    public virtual void Eat()
+    {
+        Console.WriteLine("Animalul mănâncă.");
+    }
+
+    public interface IRunnable
+    {
+        void Run();
+    }
+
+}
---
+++ Zoo/Zoo/Exemplu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using static Animal;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+
+        dog.MakeSound();
+        dog.Eat(); 
+
+        cat.MakeSound();
+        cat.Eat(); 
+
+        IRunnable runningDog = new Dog();
+        IRunnable runningCat = new Cat();
+
+        runningDog.Run(); 
+        runningCat.Run(); 
+    }
+}
