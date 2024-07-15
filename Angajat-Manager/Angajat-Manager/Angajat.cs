bc3bfb96b6f7c34de21ecc93aa2c5133735b8e6c
axelpxl33 <akselnordevpxl33@gmail.com>
2024-07-15 16:04:28 +0300

    commit angajat manager

---
+++ Angajat-Manager/Angajat-Manager/Angajat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Angajat
+{
+    public string Nume { get; set; }
+
+    public Angajat(string nume)
+    {
+        Nume = nume;
+    }
+
+    public virtual void AfisareDetalii()
+    {
+        Console.WriteLine($"Angajatul: {Nume}");
+    }
+}
---
+++ Angajat-Manager/Angajat-Manager/Manager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Manager : Angajat
+{
+    public string Departament { get; set; }
+
+    public Manager(string nume, string departament) : base(nume)
+    {
+        Departament = departament;
+    }
+
+    public override void AfisareDetalii()
+    {
+        Console.WriteLine($"Managerul {Nume} din departamentul {Departament}");
+    }
+}
